a9688054f13264258ad34a8b914bea6bb5a3ca62
ritsu <ritsu.21.ctws@gmail.com>
2025-01-02 21:12:29 +0900

    テストが通るよう調整

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { HStack } from "@chakra-ui/react";
 function App() {
   return (
     <>
+      <div data-testid="title">タイトル</div>
       <HStack>
         <Button>Click me</Button>
         <Button>Click me</Button>
