8e3e0abadf5b2bfb1d6d923460ff16bc35e16c81
ritsu <ritsu.21.ctws@gmail.com>
2025-01-03 10:54:26 +0900

    React Routerの導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import { Provider } from "@/components/ui/provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>,
 );
