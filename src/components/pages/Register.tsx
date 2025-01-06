f06309ee1f8d056b4ad3af2149dd9baf3c5096d0
ritsu <ritsu.21.ctws@gmail.com>
2025-01-06 12:39:35 +0900

    名刺登録ページのタイトルを表示

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -1,5 +1,12 @@
-import React, { memo } from "react";
+import React, { memo } from 'react';
+import { Card, Center, Heading, Spinner } from '@chakra-ui/react';
 
 export const Register: React.FC = memo(() => {
-  return <p>Registerページです</p>;
+  return (
+    <>
+      <Center mt="10">
+        <Heading as="h1">名刺新規登録</Heading>
+      </Center>
+    </>
+  );
 });
