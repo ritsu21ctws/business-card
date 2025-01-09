d86516aab0e1af7d46c562b52caae6d58f836e38
ritsu <ritsu.21.ctws@gmail.com>
2025-01-09 19:17:54 +0900

    テスト時のChakra UIのSelectの型エラーを解消

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import type { CollectionItem } from '@chakra-ui/react';
+import type { CollectionItem, SelectItemProps } from '@chakra-ui/react';
 import { Select as ChakraSelect, Portal } from '@chakra-ui/react';
 import { CloseButton } from './close-button';
 import * as React from 'react';
 
 interface SelectTriggerProps extends ChakraSelect.ControlProps {
   clearable?: boolean;
+  children?: React.ReactNode;
 }
 
 export const SelectTrigger = React.forwardRef<HTMLButtonElement, SelectTriggerProps>(function SelectTrigger(props, ref) {
@@ -33,6 +34,7 @@ const SelectClearTrigger = React.forwardRef<HTMLButtonElement, ChakraSelect.Clea
 interface SelectContentProps extends ChakraSelect.ContentProps {
   portalled?: boolean;
   portalRef?: React.RefObject<HTMLElement>;
+  children?: React.ReactNode;
 }
 
 export const SelectContent = React.forwardRef<HTMLDivElement, SelectContentProps>(function SelectContent(props, ref) {
@@ -46,7 +48,13 @@ export const SelectContent = React.forwardRef<HTMLDivElement, SelectContentProps
   );
 });
 
-export const SelectItem = React.forwardRef<HTMLDivElement, ChakraSelect.ItemProps>(function SelectItem(props, ref) {
+interface NewSelectItemProps extends SelectItemProps {
+  children?: React.ReactNode;
+  item: CollectionItem;
+  key?: string;
+}
+
+export const SelectItem = React.forwardRef<HTMLDivElement, NewSelectItemProps>(function SelectItem(props, ref) {
   const { item, children, ...rest } = props;
   return (
     <ChakraSelect.Item key={item.value} item={item} {...rest} ref={ref}>
@@ -58,6 +66,7 @@ export const SelectItem = React.forwardRef<HTMLDivElement, ChakraSelect.ItemProp
 
 interface SelectValueTextProps extends Omit<ChakraSelect.ValueTextProps, 'children'> {
   children?(items: CollectionItem[]): React.ReactNode;
+  placeholder: string;
 }
 
 export const SelectValueText = React.forwardRef<HTMLSpanElement, SelectValueTextProps>(function SelectValueText(props, ref) {
