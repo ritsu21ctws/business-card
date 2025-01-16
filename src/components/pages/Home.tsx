af48cb8e915c0170b555c539b7b43cc5459aaf2e
ritsu <ritsu.21.ctws@gmail.com>
2025-01-16 23:04:40 +0900

    Home画面のタイトルの実装

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,12 @@
 import React, { memo } from 'react';
+import { Center, Heading } from '@chakra-ui/react';
 
 export const Home: React.FC = memo(() => {
-  return <p data-testid="title">Homeページです</p>;
+  return (
+    <Center my="5">
+      <Heading as="h1" data-testid="title">
+        デジタル名詞アプリ
+      </Heading>
+    </Center>
+  );
 });
