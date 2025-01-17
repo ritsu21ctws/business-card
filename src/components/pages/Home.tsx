7a1fa14dcf23c98f4d934759f982e7c8234da3c7
ritsu <ritsu.21.ctws@gmail.com>
2025-01-17 23:13:38 +0900

    IDの入力フォームの表示

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,27 @@
 import React, { memo } from 'react';
-import { Center, Heading } from '@chakra-ui/react';
+import { Button, Card, Center, Heading, Input, Stack } from '@chakra-ui/react';
+import { Field } from '@/components/ui/field';
 
 export const Home: React.FC = memo(() => {
   return (
     <Center my="5">
-      <Heading as="h1" data-testid="title">
-        デジタル名詞アプリ
-      </Heading>
+      <Stack>
+        <Heading as="h1" mb="2" textAlign="center" data-testid="title">
+          デジタル名詞アプリ
+        </Heading>
+        <Card.Root width="340px" variant="elevated">
+          <Card.Body>
+            <Field label="ID">
+              <Input />
+            </Field>
+          </Card.Body>
+          <Card.Footer justifyContent="flex-end">
+            <Button variant="solid" type="submit" aria-label="Submit" colorPalette="cyan" w="full">
+              名刺をみる
+            </Button>
+          </Card.Footer>
+        </Card.Root>
+      </Stack>
     </Center>
   );
 });
