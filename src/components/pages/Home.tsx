c2b0f6707b1fbee96240bebb0cc4770d402185c0
ritsu <ritsu.21.ctws@gmail.com>
2025-01-18 13:21:32 +0900

    ページ遷移機能とバリデーションの実装

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,25 @@
 import React, { memo } from 'react';
+import { useNavigate } from 'react-router';
+import { Controller, useForm } from 'react-hook-form';
 import { Button, Card, Center, Heading, Input, Stack } from '@chakra-ui/react';
 import { Field } from '@/components/ui/field';
 
 export const Home: React.FC = memo(() => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      id: '',
+    },
+  });
+  const navigate = useNavigate();
+
+  const onSubmit = handleSubmit((data) => {
+    navigate(`/cards/${data.id}`);
+  });
+
   return (
     <Center my="5">
       <Stack>
@@ -10,16 +27,25 @@ export const Home: React.FC = memo(() => {
           デジタル名詞アプリ
         </Heading>
         <Card.Root width="340px" variant="elevated">
-          <Card.Body>
-            <Field label="ID">
-              <Input />
-            </Field>
-          </Card.Body>
-          <Card.Footer justifyContent="flex-end">
-            <Button variant="solid" type="submit" aria-label="Submit" colorPalette="cyan" w="full">
-              名刺をみる
-            </Button>
-          </Card.Footer>
+          <form onSubmit={onSubmit}>
+            <Card.Body>
+              <Field label="ID" invalid={!!errors.id} errorText={errors.id?.message}>
+                <Controller
+                  name="id"
+                  control={control}
+                  rules={{
+                    required: 'IDの入力は必須です',
+                  }}
+                  render={({ field }) => <Input {...field} />}
+                />
+              </Field>
+            </Card.Body>
+            <Card.Footer>
+              <Button variant="solid" type="submit" aria-label="Submit" colorPalette="cyan" w="full">
+                名刺をみる
+              </Button>
+            </Card.Footer>
+          </form>
         </Card.Root>
       </Stack>
     </Center>
