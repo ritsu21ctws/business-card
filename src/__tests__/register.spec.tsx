4cacfb81ed42f0e4e16c819f4402d27e4c2b0dbb
ritsu <ritsu.21.ctws@gmail.com>
2025-01-23 23:36:27 +0900

    名刺登録ページのテスト：全項目入力後のページ遷移の確認

--- src/__tests__/register.spec.tsx
+++ src/__tests__/register.spec.tsx
@@ -1,46 +1,48 @@
 import App from '../App';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { MemoryRouter } from 'react-router';
-import { User } from '@/domain/user';
 import { Skill } from '@/domain/skill';
 
-const mockFetchUser = jest.fn();
 const mockFetchSkills = jest.fn();
+const mockInsertUser = jest.fn();
+const mockShowMessage = jest.fn();
 const mockNavigator = jest.fn();
 
-jest.mock('@/utils/supabaseFunctions', () => {
-  return {
-    fetchUser: () => mockFetchUser(),
-    fetchSkills: () => mockFetchSkills(),
-  };
-});
+jest.mock('@/utils/supabaseFunctions', () => ({
+  fetchSkills: () => mockFetchSkills(),
+  insertUser: () => mockInsertUser(),
+}));
+
+jest.mock('@/hooks/useMessage', () => ({
+  useMessage: () => ({
+    showMessage: mockShowMessage,
+  }),
+}));
 
 jest.mock('react-router', () => ({
   ...jest.requireActual('react-router'),
   useNavigate: () => mockNavigator,
 }));
 
+window.ResizeObserver =
+  window.ResizeObserver ||
+  jest.fn().mockImplementation(() => ({
+    disconnect: jest.fn(),
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+  }));
+
+Element.prototype.scrollTo = jest.fn();
+
 describe('Register', () => {
   beforeEach(() => {
     (globalThis as any).IS_REACT_ACT_ENVIRONMENT = false;
-    mockFetchUser.mockResolvedValue(
-      User.createUser(
-        'tea',
-        'John Doe',
-        'A passionate software developer.',
-        [
-          { id: 1, name: 'React' },
-          { id: 3, name: 'GitHub' },
-        ],
-        'johndoe_x',
-        'johndoe_qiita',
-        'johndoe_x',
-        '2025-01-02T12:17:25.278697+00:00'
-      )
-    );
 
     mockFetchSkills.mockResolvedValue([new Skill(1, 'React'), new Skill(2, 'TypeScript'), new Skill(3, 'GitHub')]);
+
+    mockInsertUser.mockResolvedValue(Promise.resolve());
 
     render(
       <MemoryRouter initialEntries={['/cards/register']}>
@@ -54,5 +56,45 @@ describe('Register', () => {
   test('タイトルが表示されていること', () => {
     const title = screen.getByTestId('title');
     expect(title).toBeInTheDocument();
+  });
+
+  test('全項目入力して登録ボタンを押すとHomeページに遷移すること', async () => {
+    // 必須項目の入力
+    await userEvent.type(screen.getByTestId('input-id'), 'tanaka');
+    await userEvent.type(screen.getByTestId('input-name'), '田中太郎');
+    await userEvent.type(screen.getByTestId('input-description'), '30歳のエンジニアです。');
+    // スキル選択
+    await userEvent.click(
+      screen.getByRole('combobox', {
+        name: '好きな技術 *',
+      })
+    );
+    await userEvent.click(screen.getByText('React'));
+    await userEvent.click(screen.getByText('TypeScript'));
+    // オプション項目の入力
+    await userEvent.type(screen.getByTestId('input-github-id'), 'tanaka_github');
+    await userEvent.type(screen.getByTestId('input-qiita-id'), 'tanaka_qiita');
+    await userEvent.type(screen.getByTestId('input-x-id'), 'tanaka_x');
+    // フォーム送信
+    const registerButton = screen.getByTestId('register-button');
+    await userEvent.click(registerButton);
+
+    // 登録成功の確認
+    await waitFor(() => {
+      expect(mockInsertUser).toHaveBeenCalledTimes(1);
+    });
+
+    // TOP画面への遷移を確認
+    await waitFor(() => {
+      expect(mockNavigator).toHaveBeenCalledWith('/');
+    });
+
+    // トーストの確認
+    await waitFor(() => {
+      expect(mockShowMessage).toHaveBeenCalledWith({
+        title: '登録が完了しました',
+        type: 'success',
+      });
+    });
   });
 });
