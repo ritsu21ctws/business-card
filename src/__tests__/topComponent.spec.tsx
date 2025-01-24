11efa212143990a6fdaf6e547f02e330fbb2fbb1
ritsu <ritsu.21.ctws@gmail.com>
2025-01-24 22:46:21 +0900

    トップページのテスト：タイトルの表示

---
+++ src/__tests__/topComponent.spec.tsx
@@ -0,0 +1,34 @@
+import App from '../App';
+import { render, screen } from '@testing-library/react';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { MemoryRouter } from 'react-router';
+
+const mockNavigator = jest.fn();
+
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useNavigate: () => mockNavigator,
+}));
+
+describe('Top', () => {
+  beforeEach(() => {
+    (globalThis as any).IS_REACT_ACT_ENVIRONMENT = false;
+
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <ChakraProvider value={defaultSystem}>
+          <App />
+        </ChakraProvider>
+      </MemoryRouter>
+    );
+  });
+
+  afterEach(() => {
+    mockNavigator.mockClear();
+  });
+
+  test('タイトルが表示されていること', () => {
+    const title = screen.getByTestId('title');
+    expect(title).toBeInTheDocument();
+  });
+});
